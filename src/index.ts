72cc3d93f1a6a6babd33c439ef395f9d168e73a5
Flo <53355483+Flo4604@users.noreply.github.com>
2024-01-29 22:33:13 +0100

    Add config file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { existsSync, writeFileSync } from "fs";
 import { EAuthTokenPlatformType, LoginSession } from "steam-session";
 import { consola } from "consola";
+import config from "../config.json";
 
 const loginData = await login();
 
@@ -11,148 +12,169 @@ if (!loginData.cookies || !loginData.tokens) {
 
 loginData.cookies.push("timezoneOffset=3600,0");
 
-const steamId = loginData.cookies
-  .find((c) => c.startsWith("steamLoginSecure"))
-  ?.split("%7C%7C")?.[0]
-  .split("=")?.[1];
+craftBoosterPacks();
 
-const sessionId = loginData.cookies
-  .find((c) => c.startsWith("sessionid"))
-  ?.split("=")?.[1];
-
-const postHeaders = {
-  Cookie: loginData.cookies.join(";"),
-  Origin: "https://steamcommunity.com",
-  Referer: "https://steamcommunity.com/tradingcards/boostercreator/",
-  Host: "steamcommunity.com",
-  UserAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-  ContentType: "application/x-www-form-urlencoded; charset=UTF-8",
-};
-
-const getHeaders = {
-  Cookie: loginData.cookies.join(";"),
-};
-
-const boosterInfo = await fetch(
-  "https://steamcommunity.com/tradingcards/boostercreator/",
-  {
-    headers: getHeaders,
-  },
-);
-
-// Regular expression to match the array in the script tag
-const regex = /CBoosterCreatorPage\.Init\(.*?(\[[^\]].*\],)/gs;
-let body = await boosterInfo.text();
-body = body.replace(/[\r\n]+/g, "");
-const match = regex.exec(body);
-
-// Check if the match was found
-if (!match || !match[1]) {
-  consola.error("App Booster Information not found or couldn't be parsed");
-  process.exit(1);
+if (config.recraftTime > 0) {
+  setInterval(
+    () => {
+      craftBoosterPacks();
+    },
+    1000 * 60 * 60 * config.recraftTime,
+  );
 }
 
-const boosterApps = JSON.parse(match[1].replace(/,\s*$/, "")) as BoosterApp[];
-const boostList = (await import("../apps.json")).default;
+async function craftBoosterPacks() {
+  const steamId = loginData.cookies
+    .find((c) => c.startsWith("steamLoginSecure"))
+    ?.split("%7C%7C")?.[0]
+    .split("=")?.[1];
 
-let appsToBoost = boosterApps.filter((app) => boostList.includes(app.appid));
+  const sessionId = loginData.cookies
+    .find((c) => c.startsWith("sessionid"))
+    ?.split("=")?.[1];
 
-consola.info(`Found ${appsToBoost.length}/${boostList.length} apps to boost.`);
+  const postHeaders = {
+    Cookie: loginData.cookies.join(";"),
+    Origin: "https://steamcommunity.com",
+    Referer: "https://steamcommunity.com/tradingcards/boostercreator/",
+    Host: "steamcommunity.com",
+    UserAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    ContentType: "application/x-www-form-urlencoded; charset=UTF-8",
+  };
 
-let i = 1;
-let gemsRemaining = 99999;
-for (const app of appsToBoost) {
-  let logStart = `${app.name} (${app.appid}) - `;
-  let logEnd = ` (${i++}/${appsToBoost.length})`;
+  const getHeaders = {
+    Cookie: loginData.cookies.join(";"),
+  };
 
-  if (app?.unavailable === true) {
-    consola.info(
-      logStart +
-        `Skipping because it is unavailable until ${app.available_at_time}` +
-        logEnd,
-    );
-    continue;
-  }
-
-  if (gemsRemaining - parseInt(app.price) < 0) {
-    consola.info(
-      logStart +
-        `Skipping because we only have ${gemsRemaining} gems left` +
-        logEnd,
-    );
-    continue;
-  }
-
-  const response = await fetch(
-    "https://steamcommunity.com/tradingcards/ajaxcreatebooster/",
+  const boosterInfo = await fetch(
+    "https://steamcommunity.com/tradingcards/boostercreator/",
     {
-      method: "POST",
-      headers: postHeaders,
-      // @ts-ignore
-      body: new URLSearchParams({
-        appid: `${app.appid}`,
-        series: "1",
-        tradability_preference: "2",
-        sessionid: sessionId,
-      }),
+      headers: getHeaders,
     },
   );
 
-  if (!response.ok) {
-    consola.error(
+  // Regular expression to match the array in the script tag
+  const regex = /CBoosterCreatorPage\.Init\(.*?(\[[^\]].*\],)/gs;
+  let body = await boosterInfo.text();
+  body = body.replace(/[\r\n]+/g, "");
+  const match = regex.exec(body);
+
+  // Check if the match was found
+  if (!match || !match[1]) {
+    consola.error("App Booster Information not found or couldn't be parsed");
+    process.exit(1);
+  }
+
+  const boosterApps = JSON.parse(match[1].replace(/,\s*$/, "")) as BoosterApp[];
+  const boostList = (await import("../apps.json")).default;
+
+  let appsToBoost = boosterApps.filter((app) => boostList.includes(app.appid));
+
+  consola.info(
+    `Found ${appsToBoost.length}/${boostList.length} apps to boost.`,
+  );
+
+  let i = 1;
+  let gemsRemaining = 99999;
+  for (const app of appsToBoost) {
+    let logStart = `${app.name} (${app.appid}) - `;
+    let logEnd = ` (${i++}/${appsToBoost.length})`;
+
+    if (app?.unavailable === true) {
+      consola.info(
+        logStart +
+          `Skipping because it is unavailable until ${app.available_at_time}` +
+          logEnd,
+      );
+      continue;
+    }
+
+    if (gemsRemaining - parseInt(app.price) < 0) {
+      consola.info(
+        logStart +
+          `Skipping because we only have ${gemsRemaining} gems left` +
+          logEnd,
+      );
+      continue;
+    }
+
+    const response = await fetch(
+      "https://steamcommunity.com/tradingcards/ajaxcreatebooster/",
+      {
+        method: "POST",
+        headers: postHeaders,
+        // @ts-ignore
+        body: new URLSearchParams({
+          appid: `${app.appid}`,
+          series: "1",
+          tradability_preference: "2",
+          sessionid: sessionId,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      consola.error(
+        logStart +
+          `Failed to create boosterPack Response: ${await response.text()} | Status: ${response.status} ${response.statusText}` +
+          logEnd,
+      );
+
+      continue;
+    }
+
+    const boosterData = (await response.json()) as CreateBoosterResponse;
+    consola.success(
       logStart +
-        `Failed to create boosterPack Response: ${await response.text()} | Status: ${response.status} ${response.statusText}` +
+        `Created Booster with ID ${boosterData?.purchase_result?.communityitemid} [${boosterData.tradable_goo_amount}] Gems remaining` +
         logEnd,
     );
 
-    continue;
-  }
+    if (!config.unpack) {
+      continue;
+    }
 
-  const boosterData = (await response.json()) as CreateBoosterResponse;
-  consola.success(
-    logStart +
-      `Created Booster with ID ${boosterData?.purchase_result?.communityitemid} [${boosterData.tradable_goo_amount}] Gems remaining` +
-      logEnd,
-  );
-
-  const unpackResponse = await fetch(
-    `https://steamcommunity.com/profiles/${steamId}/ajaxunpackbooster/`,
-    {
-      method: "POST",
-      headers: postHeaders,
-      // @ts-ignore
-      body: new URLSearchParams({
-        appid: app.appid,
-        communityitemid: boosterData?.purchase_result?.communityitemid,
-        sessionid: sessionId,
-      }),
-    },
-  );
-
-  const unpackData = (await unpackResponse.json()) as UnpackBoosterResponse;
-
-  if (!unpackResponse.ok || !unpackData.success) {
-    consola.error(
-      logStart +
-        `Failed to unpack boosterPack Response: ${await response.text()} | Status: ${response.status} ${response.statusText}` +
-        logEnd,
+    const unpackResponse = await fetch(
+      `https://steamcommunity.com/profiles/${steamId}/ajaxunpackbooster/`,
+      {
+        method: "POST",
+        headers: postHeaders,
+        // @ts-ignore
+        body: new URLSearchParams({
+          appid: app.appid,
+          communityitemid: boosterData?.purchase_result?.communityitemid,
+          sessionid: sessionId,
+        }),
+      },
     );
 
-    continue;
+    const unpackData = (await unpackResponse.json()) as UnpackBoosterResponse;
+
+    if (!unpackResponse.ok || !unpackData.success) {
+      consola.error(
+        logStart +
+          `Failed to unpack boosterPack Response: ${await response.text()} | Status: ${response.status} ${response.statusText}` +
+          logEnd,
+      );
+
+      continue;
+    }
+
+    const foils = unpackData.rgItems.filter((item) => item.foil);
+
+    consola.success(
+      logStart +
+        `Unpacked Booster with ID ${boosterData?.purchase_result?.communityitemid} Got ${
+          foils.length
+            ? `(${foils.map((foil) => foil.name).join(", ")}`
+            : foils.length
+        } foils` +
+        logEnd,
+    );
   }
 
-  const foils = unpackData.rgItems.filter((item) => item.foil);
-
-  consola.success(
-    logStart +
-      `Unpacked Booster with ID ${boosterData?.purchase_result?.communityitemid} Got ${
-        foils.length
-          ? `(${foils.map((foil) => foil.name).join(", ")}`
-          : foils.length
-      } foils` +
-      logEnd,
-  );
+  return true;
 }
 
 async function login(): Promise<{
@@ -225,14 +247,14 @@ async function writeSession(session: LoginSession) {
   return { tokens: data, cookies: await session.getWebCookies() };
 }
 
-export interface CreateBoosterResponse {
+interface CreateBoosterResponse {
   purchase_result: PurchaseResult;
   goo_amount: string;
   tradable_goo_amount: string;
   untradable_goo_amount: number;
 }
 
-export interface PurchaseResult {
+interface PurchaseResult {
   communityitemid: string;
   appid: number;
   item_type: number;
@@ -241,7 +263,7 @@ export interface PurchaseResult {
   rwgrsn: number;
 }
 
-export interface UnpackBoosterResponse {
+interface UnpackBoosterResponse {
   success: number;
   rgItems: RgItem[];
 }
